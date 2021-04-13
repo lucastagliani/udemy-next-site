0c4ac5597b1619b3fa9b82d375d61ce913c89ab7
Lucas Tagliani <lucas.tagliani@outlook.com>
2021-04-13 02:56:08 -0300

    section 2: setup app and repository complete

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Header from '@components/header'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>All about NextJS</title>
+      </Head>
+
+      <main>
+        <Header />
+      </main>
+    </>
+  )
+}
---
+++ shared/globals.ts
@@ -0,0 +1,29 @@
+import { css } from '@emotion/react'
+
+export const globalStyles = css`
+  :root {
+    --page-background: #222;
+    --page-text: #ddd;
+  }
+
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    background-color: var(--page-background);
+    color: var(--page-text);
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  *,
+  *::after,
+  *::before {
+    box-sizing: border-box;
+  }
+`
